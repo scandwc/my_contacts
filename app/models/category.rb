64fa8612e91662d3fa4df80041e0c5a2cb1b7476
Raghu Betina <raghu@firstdraft.com>
2018-06-07 21:06:20 +0000

    Direct association between categories and contacts

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   # Direct associations
 
+  has_many   :contacts,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 class Contact < ApplicationRecord
   # Direct associations
 
+  belongs_to :category
+
   has_many   :comments,
              :dependent => :destroy
 
