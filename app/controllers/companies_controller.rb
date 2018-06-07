b5e125d09b006d9a5ff0526fe72081652cea3458
Raghu Betina <raghu@firstdraft.com>
2018-06-07 21:05:51 +0000

    Generate companies resource with starter:resource

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,81 @@
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+
+    render("companies/index.html.erb")
+  end
+
+  def show
+    @company = Company.find(params[:id])
+
+    render("companies/show.html.erb")
+  end
+
+  def new
+    @company = Company.new
+
+    render("companies/new.html.erb")
+  end
+
+  def create
+    @company = Company.new
+
+    @company.name = params[:name]
+    @company.description = params[:description]
+
+    save_status = @company.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/companies/new", "/create_company"
+        redirect_to("/companies")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Company created successfully.")
+      end
+    else
+      render("companies/new.html.erb")
+    end
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+
+    render("companies/edit.html.erb")
+  end
+
+  def update
+    @company = Company.find(params[:id])
+
+    @company.name = params[:name]
+    @company.description = params[:description]
+
+    save_status = @company.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/companies/#{@company.id}/edit", "/update_company"
+        redirect_to("/companies/#{@company.id}", :notice => "Company updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Company updated successfully.")
+      end
+    else
+      render("companies/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+
+    @company.destroy
+
+    if URI(request.referer).path == "/companies/#{@company.id}"
+      redirect_to("/", :notice => "Company deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Company deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Company resource:
+  # CREATE
+  get "/companies/new", :controller => "companies", :action => "new"
+  post "/create_company", :controller => "companies", :action => "create"
+
+  # READ
+  get "/companies", :controller => "companies", :action => "index"
+  get "/companies/:id", :controller => "companies", :action => "show"
+
+  # UPDATE
+  get "/companies/:id/edit", :controller => "companies", :action => "edit"
+  post "/update_company/:id", :controller => "companies", :action => "update"
+
+  # DELETE
+  get "/delete_company/:id", :controller => "companies", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
